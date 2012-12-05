8e7415a6bd7414f474cd4fe2f9f68cb9be08abc6
Timo Stollenwerk <contact@timostollenwerk.net>
2012-12-05 10:54:08 +0100

    Prepare new release (mo files were missing).

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0.2'
+version = '1.0.3'
 description = 'MailChimp integration for Plone.'
 long_description = \
     open("README.txt").read() + "\n" + \
