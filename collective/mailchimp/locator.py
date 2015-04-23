5fb923c8da79ab7cf78d21430f2259985bfbfd9d
Maurits van Rees <maurits@vanrees.org>
2015-04-23 13:14:47 +0200

    Fix utf-8 python coding header.
    My editor complained about the uppercase UTF-8.

--- collective/mailchimp/locator.py
+++ collective/mailchimp/locator.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 from postmonkey import PostMonkey
 from collective.mailchimp.interfaces import IMailchimpSettings
 from zope.component import getUtility
