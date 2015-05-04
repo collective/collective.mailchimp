9f5325fba76f49090650a848e7aaf961b0a90df5
Timo Stollenwerk <tisto@plone.org>
2015-05-04 17:00:42 +0200

    Back to development: 1.4.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.4.1'
+version = '1.4.2.dev0'
 description = 'MailChimp integration for Plone.'
 long_description = \
     open("README.rst").read() + "\n" + \
