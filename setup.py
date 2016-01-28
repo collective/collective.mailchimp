efac48e86b54b4eaa33d59104b0f3834288ca5a2
Timo Stollenwerk <tisto@plone.org>
2016-01-28 20:34:08 +0100

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.mailchimp',
-    version='2.0',
+    version='2.0.1.dev0',
     description="MailChimp integration for Plone.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
