7e32a034ee5bbccfd3f4908299a68d749d38b740
Maurits van Rees <maurits@vanrees.org>
2016-02-02 11:13:25 +0100

    Back to development: 2.0.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.mailchimp',
-    version='2.0.2',
+    version='2.0.3.dev0',
     description="MailChimp integration for Plone.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
